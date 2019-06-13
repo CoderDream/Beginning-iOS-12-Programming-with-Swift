5e13d1a79f5e18af55150bc1b75335c74f5f0ba8
CoderDream <coderdream@gmail.com>
2019-06-13 22:41:45 +0800

    Chapter 6 Designing UI Using Stack Views 20190613

---
+++ Chapter06/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by CoderDream on 2019/6/13.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SourceCode/Chapter06/StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Simon Ng on 6/8/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
