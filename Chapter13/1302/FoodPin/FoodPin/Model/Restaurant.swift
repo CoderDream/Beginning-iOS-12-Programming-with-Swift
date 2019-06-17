fdbd4113142a165d0ca3ca54b6aa0403d730de29
CoderDream <coderdream@gmail.com>
2019-06-17 15:37:07 +0800

    1303 Organizing Project Files 20190617

---
+++ Chapter13/1302/FoodPin/FoodPin/Model/Restaurant.swift
@@ -0,0 +1,29 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/17.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isVisited: Bool
+    
+    init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isVisited = isVisited
+    }
+    
+    convenience init() {
+        self.init(name: "", type: "", location: "", image: "", isVisited: false)
+    }
+}
