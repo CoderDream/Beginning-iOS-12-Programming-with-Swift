b1066914b210eaaa98b23db0049333cb5c1b1d81
CoderDream <coderdream@gmail.com>
2019-06-17 17:12:40 +0800

    1401 Designing the Table View Header 20190617

---
+++ Chapter14/1401/FoodPinDetailStarter/FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/17.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var heartImageView: UIImageView!
+}
