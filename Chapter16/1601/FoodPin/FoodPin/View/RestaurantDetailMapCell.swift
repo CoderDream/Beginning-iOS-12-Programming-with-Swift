2da4360055ae17ddab766707f76c5f19265a547e
CoderDream <coderdream@gmail.com>
2019-06-19 17:19:59 +0800

    1601 Adding a Map Interface to Your App

---
+++ Chapter16/1601/FoodPin/FoodPin/View/RestaurantDetailMapCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantDetailMapCell.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RestaurantDetailMapCell: UITableViewCell {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
