f9dd313998b2920149ad94ec5a26cae0fcef3e37
CoderDream <coderdream@gmail.com>
2019-06-20 17:11:01 +0800

    1704 Spring Animation 20190620

---
+++ Chapter17/1704/FoodPin/FoodPin/View/RestaurantDetailTextCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/18.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+    
+    @IBOutlet var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.numberOfLines = 0
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
