cc25e5cc26f6235135de170b56c6eb38dba55cd0
CoderDream <coderdream@gmail.com>
2019-06-22 22:42:45 +0800

    1801 Designing the New Restaurant View Controller 20190622

---
+++ Chapter18/1801/FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,162 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 13/8/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    
+    var restaurant = Restaurant()
+    
+    // MARK: - View controller life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // Configure header view
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
+        
+        // Set the table view's delegate and data source
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Configure the table view's style
+        tableView.separatorStyle = .none
+        
+        
+//        // Customize the navigation bar
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+//        navigationController?.navigationBar.shadowImage = UIImage()
+//        navigationController?.navigationBar.tintColor = .white
+        
+        //tableView.contentInsetAdjustmentBehavior = .never
+        
+        // Customize the navigation bar
+        //navigationController?.navigationBar.backgroundColor = .gray
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.hidesBarsOnSwipe = false
+        
+        tableView.contentInsetAdjustmentBehavior = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnSwipe = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func close(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func rateRestaurant(segue: UIStoryboardSegue) {
+       dismiss(animated: true, completion: {
+            if let rating = segue.identifier {
+                self.restaurant.rating = rating
+                self.headerView.ratingImageView.image = UIImage(named: rating)
+                
+                let scaleTransform = CGAffineTransform.init(scaleX: 0.1, y: 0.1)
+                self.headerView.ratingImageView.transform = scaleTransform
+                self.headerView.ratingImageView.alpha = 0
+
+                UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.7, options: [], animations: {
+                    self.headerView.ratingImageView.transform = .identity
+                    self.headerView.ratingImageView.alpha = 1
+                }, completion: nil)
+            }
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - UITableViewDataSource methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+            
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(named: "phone")
+            cell.shortTextLabel.text = restaurant.phone
+            cell.selectionStyle = .none
+            
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(named: "map")
+            cell.shortTextLabel.text = restaurant.location
+            cell.selectionStyle = .none
+            
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            cell.selectionStyle = .none
+            
+            return cell
+        case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailSeparatorCell.self), for: indexPath) as! RestaurantDetailSeparatorCell
+            cell.titleLabel.text = "HOW TO GET HERE"
+            cell.selectionStyle = .none
+            
+            return cell            
+        case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self), for: indexPath) as! RestaurantDetailMapCell
+            cell.selectionStyle = .none
+            cell.configure(location: restaurant.location)
+            
+            return cell            
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
+        
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMap" {
+            let destinationController = segue.destination as! MapViewController
+            destinationController.restaurant = restaurant
+        } else if segue.identifier == "showReview" {
+            let destinationController = segue.destination as! ReviewViewController
+            destinationController.restaurant = restaurant
+        }
+    }
+    
+    // MARK: - Status bar
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
---
+++ Chapter18/1801/FoodPin/FoodPin/Controller/ReviewViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/20.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var rateButtons: [UIButton]!
+    @IBOutlet var closeButton: UIButton!
+    
+    var restaurant = Restaurant()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.        
+        backgroundImageView.image = UIImage(named: restaurant.image)
+        
+        // Applying the blur effect
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
+        let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        
+        // Make the button invisible and move off the screen
+        for rateButton in rateButtons {
+            rateButton.transform = moveScaleTransform
+            rateButton.alpha = 0
+        }
+        
+        // Move up the closee button
+        let moveUpTransform = CGAffineTransform.init(translationX: 0, y: -400)
+        closeButton.transform = moveUpTransform
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        for index in 0...4 {
+            UIView.animate(withDuration: 0.4, delay: (0.1 + 0.05 * Double(index)), options: [], animations: {
+                self.rateButtons[index].alpha = 1.0
+                self.rateButtons[index].transform = .identity
+            }, completion: nil)
+        }
+//
+//        UIView.animate(withDuration: 0.4) {
+//            self.closeButton.transform = .identity
+//        }
+        
+//        UIView.animate(withDuration: 2.0) {
+//            for index in 0...4 {
+//                self.rateButtons[index].alpha = 1.0
+//            }
+//        }
+//        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+//            self.rateButtons[0].alpha = 1.0
+//            self.rateButtons[0].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+//            self.rateButtons[1].alpha = 1.0
+//            self.rateButtons[1].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+//            self.rateButtons[2].alpha = 1.0
+//            self.rateButtons[2].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+//            self.rateButtons[3].alpha = 1.0
+//            self.rateButtons[3].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+//            self.rateButtons[4].alpha = 1.0
+//            self.rateButtons[4].transform = .identity
+//        }, completion: nil)
+        
+//        UIView.animate(withDuration: 1.0, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+//            self.rateButtons[0].alpha = 1.0
+//            self.rateButtons[0].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+//            self.rateButtons[1].alpha = 1.0
+//            self.rateButtons[1].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+//            self.rateButtons[2].alpha = 1.0
+//            self.rateButtons[2].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+//            self.rateButtons[3].alpha = 1.0
+//            self.rateButtons[3].transform = .identity
+//        }, completion: nil)
+//
+//        UIView.animate(withDuration: 1.0, delay: 0.1, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.3, options: [], animations: {
+//            self.rateButtons[4].alpha = 1.0
+//            self.rateButtons[4].transform = .identity
+//        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.6) {
+            self.closeButton.transform = .identity
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
