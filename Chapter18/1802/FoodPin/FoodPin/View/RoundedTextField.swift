76a56f6c2b37d376d8f2cab02c21eef6efa2891e
CoderDream <coderdream@gmail.com>
2019-06-22 23:18:15 +0800

    1802 Building a Rounded Corner Text Field 20190622

---
+++ Chapter18/1802/FoodPin/FoodPin/View/RoundedTextField.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedTextField.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/22.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RoundedTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
