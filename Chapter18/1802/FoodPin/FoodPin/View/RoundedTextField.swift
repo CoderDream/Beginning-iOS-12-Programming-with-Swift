b0fe8d1a1499a1f84404acce8322f9c47a8c2841
CoderDream <coderdream@gmail.com>
2019-06-22 23:19:16 +0800

    1802 Building a Rounded Corner Text Field 20190622

--- Chapter18/1802/FoodPin/FoodPin/View/RoundedTextField.swift
+++ Chapter18/1802/FoodPin/FoodPin/View/RoundedTextField.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class RoundedTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // 左右两边各 padding 10 points 点
+    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10);
+    
+    // 文本矩形的大小
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
     }
-    */
-
+    
+    // 提示信息的矩形大小
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    // 可输入的编辑框矩形大小
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // 圆角半径 5.0
+        self.layer.cornerRadius = 5.0
+        // 设置 告诉layer将位于它之下的layer都遮盖住
+        self.layer.masksToBounds = true
+    }
 }
