f000822f59517de0eb3cfa769590d7e512c9a900
CoderDream <coderdream@gmail.com>
2019-06-23 19:11:29 +0800

    1805 Save button and Form validation 20190623

---
+++ Chapter18/1805/FoodPin/FoodPin/View/RestaurantDetailIconTextCell.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantDetailIconTextCell.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/18.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailIconTextCell: UITableViewCell {
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var shortTextLabel: UILabel! {
+        didSet {
+            shortTextLabel.numberOfLines = 0
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
