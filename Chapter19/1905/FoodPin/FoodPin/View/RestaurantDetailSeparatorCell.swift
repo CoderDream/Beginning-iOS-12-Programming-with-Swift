6182ebc8eebc63bf883a240239024c73da34b8d9
CoderDream <coderdream@gmail.com>
2019-06-23 22:55:21 +0800

    1905 Updating a Managed Object 20190623

---
+++ Chapter19/1905/FoodPin/FoodPin/View/RestaurantDetailSeparatorCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantDetailSeparatorCell.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailSeparatorCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
