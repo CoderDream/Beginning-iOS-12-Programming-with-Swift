245498eeeaad5cc9c9cab1cf6843b6ceff1f20e1
CoderDream <coderdream@gmail.com>
2019-06-25 14:39:29 +0800

    2303 Loading Web Content Using WKWebView 20190625

---
+++ Chapter23/2303/FoodPin/FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by CoderDream on 2019/6/17.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel! {
+        didSet {
+            nameLabel.numberOfLines = 0
+        }
+    }
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            typeLabel.layer.cornerRadius = 5.0
+            typeLabel.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet var heartImageView: UIImageView! {
+        didSet {
+            heartImageView.image = UIImage(named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+            heartImageView.tintColor = .white
+        }
+    }
+        
+    @IBOutlet var ratingImageView: UIImageView!
+}
